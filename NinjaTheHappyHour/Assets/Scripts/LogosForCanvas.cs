2a002c33c9b47539ed093aab43fe8728a551778c
Lucas Martines <lucas.alcantarilla97@gmail.com>
2017-02-24 14:14:42 -0300

    Adicionado script fade in fadeout para canvas

--- NinjaTheHappyHour/Assets/Scripts/LogosForCanvas.cs
+++ NinjaTheHappyHour/Assets/Scripts/LogosForCanvas.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class StartLogos : MonoBehaviour {
+public class LogosForCanvas : MonoBehaviour
+{
     // Classe que cria o efeito fade in e fade out
     public int SceneIndexToChangeScene = 2;
     public bool setScene = false;
@@ -18,31 +19,31 @@ public class StartLogos : MonoBehaviour {
     public float fadeInTime = 1000;// atenção  o fade in time também é usado como start time diferente do fade out que tem 2 variaveis uma start e uma time
     public float fadeOutStartTime = 3000;
     public float fadeOutTime = 1000;
-    private SpriteRenderer render;
+    private Image render;
     public float colorAlpha;
     private float proporcaoFadeIn;// é a procorção para converter o delta tempo de start e limit por conter
     private float proporcaoFadeOut;
 
-    private Color32 FadeInColor = new Color32(255,255,255, 0 );
-    private Color32 FadeOutColor = new Color32(255, 255,255, 0);
+    private Color32 FadeInColor = new Color32(255, 255, 255, 0);
+    private Color32 FadeOutColor = new Color32(255, 255, 255, 0);
 
     void Start()
     {
 
-        
-        
-        fadeOutStartTime = TimeApresentacao + fadeInTime ;
 
-        fadeOutConter = ( (fadeOutStartTime - TimeApresentacao) / 1000 ) ;
 
-        proporcaoFadeIn = 255 / (fadeInTime );// assim o tempo é relacionado com os bits
+        fadeOutStartTime = TimeApresentacao + fadeInTime;
+
+        fadeOutConter = ((fadeOutStartTime - TimeApresentacao) / 1000);
+
+        proporcaoFadeIn = 255 / (fadeInTime);// assim o tempo é relacionado com os bits
         proporcaoFadeOut = 255 / (fadeOutTime);
 
 
         print("Proporção de tempo" + proporcaoFadeIn);
 
-        conter = startTime/1000; // converter valor start em 1000
-        render = GetComponent<SpriteRenderer>();
+        conter = startTime / 1000; // converter valor start em 1000
+        render = GetComponent<Image>();
         render.color = FadeInColor;
     }
 
@@ -50,27 +51,27 @@ public class StartLogos : MonoBehaviour {
     void Update()
     {
         // atualiza o contador adicionando tempo
-       //print( "os bits "+ setAlphaBits());
+        //print( "os bits "+ setAlphaBits());
         timeFadeInApresentOut();
         // UpdateConterDecrement();
 
-        
+
 
     }
-    Byte setAlphaBitsFade(float conter,float proporcao,String fadeTag)
+    Byte setAlphaBitsFade(float conter, float proporcao, String fadeTag)
     {// função que não permite que alpha chege a 256 estourando 8 bits
-        colorAlpha = proporcao  * conter;
+        colorAlpha = proporcao * conter;
 
         colorAlpha *= 1000;// restaura uma proporção que não será destruida caso 0.255 então é 0 bits de alpha 
         // para resolver o erro de conversão de float para bits é só multiplicar por 1000     
         if (colorAlpha > 255)
         {
-            print("passou 255 "+ fadeTag);
+            print("passou 255 " + fadeTag);
             colorAlpha = 255;
         }
-        if(colorAlpha < 0 || (colorAlpha > 0 && colorAlpha < 5))
+        if (colorAlpha < 0 || (colorAlpha > 0 && colorAlpha < 5))
         {
-            print("passou < 0"+fadeTag);
+            print("passou < 0" + fadeTag);
             colorAlpha = 0;
         }
 
@@ -80,14 +81,16 @@ public class StartLogos : MonoBehaviour {
     void timeFadeInApresentOut()
     {
 
-        
+
         // fade in
-        if (tmpConter < startTime / 1000){
+        if (tmpConter < startTime / 1000)
+        {
             print("delay");
             tmpConter += Time.deltaTime;
 
         }
-        else{
+        else
+        {
 
             if (conter >= startTime / 1000 && conter < (FadeInEndTime + startTime) / 1000)// converte segundos em milesimos
             {
@@ -113,9 +116,9 @@ public class StartLogos : MonoBehaviour {
                 FadeOutColor.a = setAlphaBitsFade(fadeOutConter, proporcaoFadeOut, "out");
                 //print("Os bits"+setAlphaBits());// 
                 render.color = FadeOutColor;// 
-               
+
             }
-            if(conter >= (TimeApresentacao + startTime + fadeOutTime)/ 1000)
+            if (conter >= (TimeApresentacao + startTime + fadeOutTime) / 1000)
             {
                 if (setScene == true)
                 {
@@ -128,6 +131,6 @@ public class StartLogos : MonoBehaviour {
 
 
     }
-   
-    
+
+
 }
